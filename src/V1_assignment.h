817de7567a4e115eaac6d43b014344f495051907
Nicholas Fox <157935022+theHDM@users.noreply.github.com>
2024-11-18 15:13:09 -0500

    Update V1_assignment.h

--- src/V1_assignment.h
+++ src/V1_assignment.h
@@ -1,4 +1,5 @@
 #pragma once
+// need to outline dependencies
 /*
   This section of the code contains broad
   procedures for assigning musical notes
@@ -9,7 +10,7 @@
 void assignPitches() {     
   sendToLog("assignPitch was called:");
   for (auto& h : hexBoard.keys) {
-    // steps is the distance from C
+    // steps is the distance from C (the anchor note)
     // the stepsToMIDI function needs distance from A4
     // it also needs to reflect any transposition, but
     // NOT the key of the scale.
@@ -19,6 +20,11 @@ void assignPitches() {
       h.bend = 0;
       h.frequency = 0.0;
     } else {
+      // h.note and h.bend may soon be collapsed into
+      // a floating h.note value, with h.bend calculated
+      // situationally.
+      // also might have to reflect global tuning (i.e.
+      // what if the KBM file defines A = 432 or A = 441
       h.note = ((N >= 127) ? 127 : round(N));
       h.bend = (ldexp(N - h.note, 13) / MPEpitchBendSemis);
       h.frequency = MIDItoFreq(N);
@@ -30,6 +36,7 @@ void applyScale() {
   sendToLog("applyScale was called:");
   for (auto& h : hexBoard.keys) {
     if (current.scale().tuning == ALL_TUNINGS) {
+      // i.e. "no scale"
       h.inScale = 1;
     } else {
       uint degree = current.keyDegree(h.stepsFromC); 
@@ -52,12 +59,17 @@ void applyScale() {
 void applyLayout() {       // call this function when the layout changes
   sendToLog("buildLayout was called:");
   hex_t middleC = hexBoard.button_at_pixel(current.layout().hexMiddleC);
-  hex_t stepVector = {
-    current.layout().acrossSteps / 2,
-    current.layout().dnLeftSteps + (current.layout().acrossSteps / 2)
-  };
+  // in orthogonal coordinates, a single hex distance = 2 steps, either
+  // +/- 2X, or +/- 1X +/- 1Y. keep the scale vector doubled so that
+  // integer values are not lost. we might change this when steps are
+  // changed to floating cents values.
+  hex_t doubleStepVector = { current.layout().acrossSteps,
+    (2 * current.layout().dnLeftSteps) + current.layout().acrossSteps};
   for (auto& h : hexBoard.keys) {
-    h.stepsFromC = dot_product(stepVector, h.coord - middleC);
+    h.stepsFromC = dot_product(doubleStepVector, h.coord - middleC) / 2;
+    // h.centsFromAnchor in the future
+    // will also require defining template<typename T> hex_t<T> to allow
+    // floating values of step-vectors.
   }
   applyScale();        // when layout changes, have to re-apply scale and re-apply LEDs
   assignPitches();     // same with pitches
