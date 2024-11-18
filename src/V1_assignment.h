0fe13b13418c9a95f465b6204193eccce7703b65
Nicholas Fox <157935022+theHDM@users.noreply.github.com>
2024-11-18 14:41:27 -0500

    Update V1_assignment.h

--- src/V1_assignment.h
+++ src/V1_assignment.h
@@ -9,21 +9,19 @@
 void assignPitches() {     
   sendToLog("assignPitch was called:");
   for (auto& h : hexBoard.keys) {
-    if (h.pixel >= 0) {
-      // steps is the distance from C
-      // the stepsToMIDI function needs distance from A4
-      // it also needs to reflect any transposition, but
-      // NOT the key of the scale.
-      float N = stepsToMIDI(current.pitchRelToA4(h.stepsFromC));
-      if (N < 0 || N >= 128) {
-        h.note = UNUSED_NOTE;
-        h.bend = 0;
-        h.frequency = 0.0;
-      } else {
-        h.note = ((N >= 127) ? 127 : round(N));
-        h.bend = (ldexp(N - h.note, 13) / MPEpitchBendSemis);
-        h.frequency = MIDItoFreq(N);
-      }
+    // steps is the distance from C
+    // the stepsToMIDI function needs distance from A4
+    // it also needs to reflect any transposition, but
+    // NOT the key of the scale.
+    float N = stepsToMIDI(current.pitchRelToA4(h.stepsFromC));
+    if (N < 0 || N >= 128) {
+      h.note = UNUSED_NOTE;
+      h.bend = 0;
+      h.frequency = 0.0;
+    } else {
+      h.note = ((N >= 127) ? 127 : round(N));
+      h.bend = (ldexp(N - h.note, 13) / MPEpitchBendSemis);
+      h.frequency = MIDItoFreq(N);
     }
   }
   sendToLog("assignPitches complete.");
@@ -31,22 +29,20 @@ void assignPitches() {
 void applyScale() {
   sendToLog("applyScale was called:");
   for (auto& h : hexBoard.keys) {
-    if (h.pixel >= 0) {
-      if (current.scale().tuning == ALL_TUNINGS) {
-        h.inScale = 1;
+    if (current.scale().tuning == ALL_TUNINGS) {
+      h.inScale = 1;
+    } else {
+      uint degree = current.keyDegree(h.stepsFromC); 
+      if (degree == 0) {
+        h.inScale = 1;    // the root is always in the scale
       } else {
-        uint8_t degree = current.keyDegree(h.stepsFromC); 
-        if (degree == 0) {
-          h.inScale = 1;    // the root is always in the scale
-        } else {
-          uint8_t tempSum = 0;
-          uint8_t iterator = 0;
-          while (degree > tempSum) {
-            tempSum += current.scale().pattern[iterator];
-            iterator++;
-          }  // add the steps in the scale, and you're in scale
-          h.inScale = (tempSum == degree);   // if the note lands on one of those sums
-        }
+        uint tempSum = 0;
+        uint iterator = 0;
+        while (degree > tempSum) {
+          tempSum += current.scale().pattern[iterator];
+          iterator++;
+        }  // add the steps in the scale, and you're in scale
+        h.inScale = (tempSum == degree);   // if the note lands on one of those sums
       }
     }
   }
@@ -56,12 +52,7 @@ void applyScale() {
 void applyLayout() {       // call this function when the layout changes
   sendToLog("buildLayout was called:");
   for (auto& h : hexBoard.keys) {
-    if (h.pixel >= 0) {
-      int8_t distCol = h.coord.x - hexBoard.button_at_pixel(current.layout().hexMiddleC).coord.x;
-      int8_t distRow = h.coord.y - hexBoard.button_at_pixel(current.layout().hexMiddleC).coord.y;
-      h.stepsFromC = ((distCol * current.layout().acrossSteps) 
-        + (distRow * (current.layout().acrossSteps + (2 * current.layout().dnLeftSteps)))) / 2;  
-    }
+    // TBD
   }
   applyScale();        // when layout changes, have to re-apply scale and re-apply LEDs
   assignPitches();     // same with pitches
